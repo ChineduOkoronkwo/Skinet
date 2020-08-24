afe7e664f7e64dbedf3f7831b226855b676264f0
Okoronkwo Alfred Chinedu <okoronkwoac@yahoo.com>
2020-08-24 11:44:32 -0400

    End of section 8

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -2,7 +2,7 @@ namespace Core.Specifications
 {
     public class ProductSpecParams
     {
-        private const int MaxPageSize = 6;
+        private const int MaxPageSize = 50;
         public int PageIndex { get; set; } = 1;
         private int _pageSize = 6;
         public int PageSize 
